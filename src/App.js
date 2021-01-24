f1e88ae5ed038981187d89f56524c7e24b050e3f
Alex Dimitrov <xavortm@gmail.com>
2021-01-24 23:39:09 +0200

    remove facebook

--- src/App.js
+++ src/App.js
@@ -19,10 +19,7 @@ const uiConfig = {
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: "/",
   // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-  ],
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
 };
 
 const UserContext = React.createContext({ loggedIn: false });
